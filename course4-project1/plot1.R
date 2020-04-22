691d2288f1c7a83968ea1bc304b008fd842219cb
thandu <tnr.mtech@gmail.com>
2020-04-22 18:45:22 +0530

    Add files via upload

---
+++ course4-project1/plot1.R
@@ -0,0 +1,21 @@
+library("data.table")
+
+setwd("~/Desktop/datasciencecoursera/course4/project1/data")
+
+powerDT <- data.table::fread(input = "household_power_consumption.txt"
+                             , na.strings="?"
+                             )
+
+powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+
+powerDT[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
+
+powerDT <- powerDT[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
+
+png("plot1.png", width=480, height=480)
+
+## Plot 1
+hist(powerDT[, Global_active_power], main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+dev.off()
